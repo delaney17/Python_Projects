3255a9b38cca8fa10da7a057bd7cff3699458680
delaney17 <108096093+delaney17@users.noreply.github.com>
2022-06-24 12:57:02 -0400

    added new directory

---
+++ madLibs/madLibs.py
@@ -0,0 +1,11 @@
+# This project prints out random strings and has the user choose certain words to fill in the blanks 
+
+adj = input("Adjective: ")
+adv1 = input("Adverb: ")
+day = input("Day of the week: ")
+favorite_food = input("Favorite Food: ")
+adv2 = input("Adverb: ")
+
+madlib = f"Working at ORNL is  {adj}! The cafeteria serves {adv1} food. My favorite day of the week is {day}. They serve {favorite_food} on {day}, and it is SO {adv2}"  
+
+print (madlib)
