7cbe44cc67f8527aa12d917c269024755fdaa369
delaney17 <108096093+delaney17@users.noreply.github.com>
2022-06-24 14:54:43 -0400

    fixed computer guess project

--- computerNumberGuess/computerGuess.py
+++ computerNumberGuess/computerGuess.py
@@ -3,18 +3,17 @@ import random
 guess = 0
 x = 0
 winningNum = int(input("What number do you choose? (between 1 and 10) "))
-# while guess != winningNum and x <= 10:
-while True: 
+while x<10: 
  guess=random.randint(1,10)
  print(f"Computer's guess: {guess}")
-#   x+=1
-# winningNum = int(input("What number do you choose? (between 1 and 10) "))
+ x+=1
  if guess == winningNum: 
-      print ("Computer got it!") 
+      print ("You lose! Computer guessed your number!") 
       break
  else:
       print ("Incorrect, guess again") 
-        
+      if x>=10:
+         print("You win! Computer failed to guess number 10 times")
 
 
 
